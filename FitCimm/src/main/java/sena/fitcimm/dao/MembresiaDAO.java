eb01644ff9b89d52391d931aa5765b58c492593b
ramosyuliana <ramosyuliana288@gmail.com>
2026-07-22 16:31:05 -0500

    Creacion y programacion de la capa Dao de planes y membresias

--- FitCimm/src/main/java/sena/fitcimm/dao/MembresiaDAO.java
+++ FitCimm/src/main/java/sena/fitcimm/dao/MembresiaDAO.java
@@ -1,5 +1,74 @@
 package sena.fitcimm.dao;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import sena.fitcimm.model.Membresia;
+import sena.fitcimm.model.Socio;
+import sena.fitcimm.util.ConexionDB;
+
 public class MembresiaDAO {
-    
+
+    public void MtInsertarMembresia(Membresia oMembresia) throws SQLException {
+        String consulta = "Insert into membresia (id_socio,id_plan,fecha_inicio,fecha_fin,valor_pagado)"
+                + "Values (?,?,?,?,?)";
+        try (Connection con = ConexionDB.getConnection(); PreparedStatement ps = con.prepareStatement(consulta)) {
+            ps.setInt(1, oMembresia.getIdSocio());
+            ps.setInt(2, oMembresia.getIdPlan());
+            ps.setDate(3, Date.valueOf(oMembresia.getFechaInicio()));
+            ps.setDate(4, Date.valueOf(oMembresia.getFechaFin()));
+            ps.setDouble(5, oMembresia.getValorPagado());
+
+        }
+
+    }
+
+    public List<Socio> MtListarMembresiasVigentes() throws SQLException {
+        List<Socio> lista = new ArrayList<>();
+        String consulta = "SELECT s.*, m.id_membresia, m.id_plan, m.fecha_inicio, m.fecha_fin, m.valor_pagado, "
+                + "FROM socio s "
+                + "INNER JOIN membresia m ON s.id_socio = m.id_socio "
+                + "WHERE s.activo = 1 AND m.fecha_fin >= CURDATE()";
+
+        try (Connection con = ConexionDB.getConnection(); PreparedStatement ps = con.prepareStatement(consulta); ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                lista.add(MtMapear(rs));
+            }
+
+        }
+        return lista;
+
+    }
+
+    private Socio MtMapear(ResultSet rs) throws SQLException {
+
+        Socio oSocio = new Socio();
+
+        oSocio.setId(rs.getInt("id_socio"));
+        oSocio.setDocumento(rs.getString("documento"));
+        oSocio.setNombres(rs.getString("nombres"));
+        oSocio.setApellidos(rs.getString("apellidos"));
+        oSocio.setTelefono(rs.getString("telefono"));
+        oSocio.setCorreo(rs.getString("correo"));
+        oSocio.setFechaNacimiento(rs.getObject("fecha_nacimiento", LocalDate.class));
+
+        Membresia oMembresia = new Membresia();
+
+        oMembresia.setId(rs.getInt("id_membresia"));
+        oMembresia.setIdSocio(rs.getInt("id_socio"));
+        oMembresia.setIdPlan(rs.getInt("id_plan"));
+        oMembresia.setFechaInicio(rs.getObject("fecha_inicio", LocalDate.class));
+        oMembresia.setFechaFin(rs.getObject("fecha_fin", LocalDate.class));
+        oMembresia.setValorPagado(rs.getDouble("valor_pagado"));
+        
+        oSocio.setMembresia(oMembresia);
+
+        return oSocio;
+    }
+
 }
--- FitCimm/src/main/java/sena/fitcimm/dao/PlanDAO.java
+++ FitCimm/src/main/java/sena/fitcimm/dao/PlanDAO.java
@@ -4,10 +4,121 @@
  */
 package sena.fitcimm.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import sena.fitcimm.model.Plan;
+import sena.fitcimm.util.ConexionDB;
+
 /**
  *
  * @author julil
  */
 public class PlanDAO {
-    
+
+    public void MtInsertarPlan(Plan oPlan) throws SQLException {
+        String consulta = "Insert into plan (nombre,duracion_dias,valor) Values (?,?,?)";
+        try (Connection con = ConexionDB.getConnection(); PreparedStatement ps = con.prepareStatement(consulta)) {
+            ps.setString(1, oPlan.getNombre());
+            ps.setInt(2, oPlan.getDuracionDias());
+            ps.setDouble(3, oPlan.getValor());
+            ps.executeUpdate();
+
+        }
+
+    }
+
+    public void MtEditarPlan(Plan oPlan) throws SQLException {
+        String consulta = "Update plan set nombre=?,duracion_dias=?,valor=? where id_plan=?";
+        try (Connection con = ConexionDB.getConnection(); PreparedStatement ps = con.prepareStatement(consulta);) {
+            ps.setString(1, oPlan.getNombre());
+            ps.setInt(2, oPlan.getDuracionDias());
+            ps.setDouble(3, oPlan.getValor());
+
+            ps.setInt(4, oPlan.getId());
+            ps.executeUpdate();
+
+        }
+
+    }
+
+    public void MtInactivarPlan(Plan oPlan) throws SQLException {
+        String consulta = "Update plan set activo=? where id_plan=?";
+        try (Connection con = ConexionDB.getConnection(); PreparedStatement ps = con.prepareStatement(consulta)) {
+            ps.setBoolean(1, oPlan.isActivo());
+            ps.executeUpdate();
+
+        }
+
+    }
+
+    public double MtTotalRecaudado(LocalDate fecha1, LocalDate fecha2) throws SQLException {
+        String consulta = "SELECT p.nombre, SUM(m.valor_pagado) AS recaudado"
+                + " FROM plan p "
+                + " INNER JOIN membresia m ON m.id_plan = p.id_plan"
+                + " WHERE m.fecha_inicio BETWEEN ? AND ?"
+                + " GROUP BY p.nombre";
+        double total = 0;
+        try (Connection con = ConexionDB.getConnection(); PreparedStatement ps = con.prepareStatement(consulta)) {
+            ps.setObject(1, fecha1);
+            ps.setObject(2, fecha2);
+         
+            try(ResultSet rs = ps.executeQuery()){
+                if(rs.next()){
+                    total = rs.getDouble("recaudado");
+                }
+            }
+            return total;
+        }
+
+    }
+
+    public String MtMasVendido() throws SQLException {
+        String consulta = "SELECT p.nombre, COUNT(m.id_membresia) AS total_vendidos "
+                + "FROM plan p "
+                + "INNER JOIN membresia m ON m.id_plan = p.id_plan "
+                + "WHERE MONTH(m.fecha_inicio) = MONTH(CURDATE()) "
+                + "AND YEAR(m.fecha_inicio) = YEAR(CURDATE()) "
+                + "GROUP BY p.nombre "
+                + "ORDER BY total_vendidos DESC "
+                + "LIMIT 1";
+        String Plan = "Sin Ventas";
+        try (Connection con = ConexionDB.getConnection(); PreparedStatement ps = con.prepareStatement(consulta);
+           
+
+            ResultSet rs = ps.executeQuery()){
+              if(rs.next()){
+                    Plan = rs.getString("nombre");
+                }
+        }
+        return Plan;
+        
+
+    }
+
+    public List<Plan> MtListarPlanes() throws Exception {
+        List<Plan> lista = new ArrayList<>();
+        String consulta = "Select * from plan";
+        try (Connection con = ConexionDB.getConnection(); PreparedStatement ps = con.prepareStatement(consulta); ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                lista.add(MtMapear(rs));
+            }
+        }
+        return lista;
+    }
+
+    public Plan MtMapear(ResultSet rs) throws SQLException {
+        Plan oPlan = new Plan();
+        oPlan.setId(rs.getInt("id_plan"));
+        oPlan.setNombre(rs.getString("nombre"));
+        oPlan.setDuracionDias(rs.getInt("duracion_dias"));
+        oPlan.setValor(rs.getDouble("valor"));
+        oPlan.setActivo(rs.getBoolean("activo"));
+        return oPlan;
+    }
+
 }
