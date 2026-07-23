132a3ea99d48665f8492a71c97894c5695559599
ramosyuliana <ramosyuliana288@gmail.com>
2026-07-22 20:43:37 -0500

    Services de membresias y de plan con sus reglas de negocio

--- FitCimm/src/main/java/sena/fitcimm/service/PlanService.java
+++ FitCimm/src/main/java/sena/fitcimm/service/PlanService.java
@@ -4,10 +4,53 @@
  */
 package sena.fitcimm.service;
 
+import java.time.LocalDate;
+import java.util.List;
+import sena.fitcimm.dao.PlanDAO;
+import sena.fitcimm.model.Plan;
+
 /**
  *
  * @author julil
  */
 public class PlanService {
     
+    private PlanDAO dao = new PlanDAO();
+    
+    public void MtRegistarPlan(Plan oPlan) throws Exception{
+        
+        if(oPlan.getNombre() == null || oPlan.getNombre().trim().isEmpty()){
+            throw new Exception("El nombre del plan no puede estar vacio");
+        }
+        
+        if(oPlan.getDuracionDias() <= 0){
+            throw new Exception("La duracion tiene que ser mayor a 0");
+        }
+        
+        if(oPlan.getValor() < 0){
+            throw new Exception("El valor del plan tiene que ser mayor a 0 ");
+        }
+        
+        dao.MtInsertarPlan(oPlan);
+    }
+    
+    public void MtEditarPlan(Plan oPlan) throws Exception{
+       dao.MtEditarPlan(oPlan);
+    }
+    
+    public void MtIntivarPlan(Plan oPlan) throws Exception{
+        dao.MtInactivarPlan(oPlan);
+    }
+    
+    public double MtTotalRecaudo(LocalDate fecha1, LocalDate fecha2) throws Exception {
+        return dao.MtTotalRecaudado(fecha1,fecha2);
+    }
+    
+     public String MtMasVendido() throws Exception {
+     return dao.MtMasVendido();
+     }
+     
+     public List<Plan> MtListarPlanes() throws Exception {
+         return dao.MtListarPlanes();
+     }
 }
