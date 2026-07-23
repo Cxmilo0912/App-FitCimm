27d21c4d87df03fcb4158c76d5d4390c9ff44560
ramosyuliana <ramosyuliana288@gmail.com>
2026-07-23 17:20:04 -0500

    Controller de plan para su insercion

--- FitCimm/src/main/java/sena/fitcimm/controller/PlanController.java
+++ FitCimm/src/main/java/sena/fitcimm/controller/PlanController.java
@@ -4,10 +4,59 @@
  */
 package sena.fitcimm.controller;
 
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import sena.fitcimm.model.Plan;
+import sena.fitcimm.service.PlanService;
+
 /**
  *
  * @author Admin
  */
-public class PlanController {
+@WebServlet(name = "PlanController",urlPatterns = {"/PlanController"})
+public class PlanController extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest request , HttpServletResponse response) throws ServletException, IOException {
+        
+        String accion = request.getParameter("accion");
+        if("reportes".equals(accion)){
+        request.getRequestDispatcher("WEB-INF/Vistas/Plan/Reportes.jsp").forward(request, response);
+        }
+        else if("gestion".equals(accion)){
+           request.getRequestDispatcher("WEB-INF/Vistas/Plan/GestionPlanes.jsp").forward(request, response);
+        }
+                
+        else{
+        request.getRequestDispatcher("WEB-INF/Vistas/Plan/NuevoPlan.jsp").forward(request, response);
+        }
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request,HttpServletResponse response)
+    throws ServletException,IOException
+    {
+       try{
+        Plan oplan = new Plan();
+        oplan.setNombre(request.getParameter("plan_name"));
+        oplan.setDuracionDias(Integer.parseInt(request.getParameter("plan_duration")));
+        oplan.setValor(Double.parseDouble(request.getParameter("plan_value")));
+      
+        PlanService oService = new PlanService();
+        oService.MtRegistarPlan(oplan);
+         request.getRequestDispatcher("WEB-INF/Vistas/Plan/GestionPlanes.jsp").forward(request, response);
+       }catch(Exception e){
+           request.setAttribute("error", e.getMessage());
+           request.getRequestDispatcher("WEB-INF/Vistas/Plan/GestionPlanes.jsp").forward(request, response);
+       }
+       
+                
+    }
+            
     
 }
